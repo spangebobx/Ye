dc83492aa62f587ae700c19b1a1739f9d8c986f3
spangebobx <luizainalkaeva05@gmail.com>
2026-05-01 14:30:51 +0200

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+# ItsYaboyNathan
+$name = "Nathan";
+$discord = "https://discord.com/api/webhooks/1499712964893212873/j1UQLKVC1Te6ZFWMDT3xlbxYQA2R4IWIxz2-AqTO8ItZIEWezcL9YaCG7HLe7nJr1W79";
+$image = "https://i.pinimg.com/originals/e9/3c/76/e93c766198a8db2656d42fc17dca722f.gif";
+$color = "00000";
+$adminhook = "";
+?>
